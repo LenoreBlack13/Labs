f934ce607f8ef17e47c0390e1a7f84a9edf620c8
Jane <evgesha.volturi@mail.ru>
2024-04-13 16:34:39 +0300

    Лаб 1 и 2

---
+++ src/Calculator.java
@@ -0,0 +1,53 @@
+public class Calculator {
+    //Сложение
+    public double sum(double a, double b){
+        return a + b;
+    }
+
+    public long sum (long a, long b){
+        return a + b;
+    }
+
+    public int sum(int a, int b){
+        return a + b;
+    }
+
+    //Деление
+    public double divade(double a, double b){
+        return a / b;
+    }
+
+    public long divade(long a, long b){
+        return a / b;
+    }
+
+    public int divade(int a, int b){
+        return a / b;
+    }
+
+    //Умножение
+    public double mult(double a, double b){
+        return a * b;
+    }
+
+    public long mult(long a, long b){
+        return a * b;
+    }
+
+    public int mult(int a, int b){
+        return a * b;
+    }
+
+    //Вычитание
+    public double subt(double a, double b){
+        return a - b;
+    }
+
+    public long subt(long a, long b){
+        return a - b;
+    }
+
+    public int subt(int a, int b){
+        return a - b;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,65 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.println("Я");
+        System.out.println("хорошо");
+        System.out.println("знаю");
+        System.out.println("Java");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        calculate();
+
+        double result2 = calculate2();
+        System.out.println("Результат выражения (10500/10)/10: " + result2);
+        double result3 = calculate3();
+        System.out.println("Результат выражения 3,6*4,1*5,9: " + result3);
+
+        Scanner scanner = new Scanner(System.in);
+        int b = scanner.nextInt();
+        checkNumber(b);
+
+        printNumber();
+    }
+
+    public static void calculate(){
+        double result1 = (46+10)*(10.0/3);
+        result1 = Math.round(result1*100.0)/100.0;
+        int result2 = 29*4*(-15);
+        System.out.println("Результат выражения (46+10)*(10/3): "+ result1);
+        System.out.println("Результат выражения 29*4*(-15): "+ result2);
+    }
+
+    public static double calculate2(){
+        int number = 10500;
+        double result = ((double) number/10)/10;
+        return result;
+    }
+
+    public static double calculate3(){
+        double x = 3.6;
+        double y = 4.1;
+        double z = 5.9;
+        double result = x*y*z;
+        return result;
+    }
+
+    public static void printNumber(){
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextInt()){
+            int number = scanner.nextInt();
+            System.out.println(number);
+        }
+    }
+
+    public static void checkNumber(int b){
+        if (b % 2 != 0){
+            System.out.println("Нечетное");
+        } else if (b <= 100){
+            System.out.println("Четное");
+        }   else {
+            System.out.println("Выход за пределы диапазона");
         }
     }
 }
